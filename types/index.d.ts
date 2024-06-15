d0864db2c77b0d94beb5c8fd16965cb6c2109cee
geo318 <geo.lomidze@gmail.com>
2024-06-15 11:59:03 +0400

    doc: update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,24 +1,17 @@
 declare global {
   interface Number {
-    add(num: number): number
-    multiply(num: number): number
-    divide(num: number): number
-    subtract(num: number): number
-    remainder(num: number): number
+    add(num: number | string, round?: number): number
+    multiply(num: number | string, round?: number): number
+    divide(num: number | string, round?: number): number
+    subtract(num: number | string, round?: number): number
+    remainder(num: number | string, round?: number): number
     percent(round?: number): number
-    format(currency?: Currency): number | string
+    format(
+      currency?: Currency | Omit<string, Currency>,
+      digits?: number,
+      locale: Intl.LocalesArgument
+    ): number | string
   }
 }
 
-declare function add(num: number | string, round?: number): number
-declare function multiply(num: number | string, round?: number): number
-declare function subtract(num: number | string, round?: number): number
-declare function remainder(num: number | string, round?: number): number
-declare function percent(round: number): number
-declare function format(a: number, b: number): number
-declare function formatNumber(
-  formatter: Intl.NumberFormat,
-  number: number
-): string
-
-export { add, multiply, subtract, remainder, percent, format, formatNumber }
+type Currency = 'EUR' | 'GEL' | 'USD' | 'RUB' | 'TRY' | 'GBP' | 'AZN'
